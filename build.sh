7fe59a4ff14a13756e85e790d15ec461c43cb3af
protoforge <protoforge@users.noreply.github.com>
2017-09-19 09:59:54 -0600

    Update build.sh
    Ensure paths are fully qualified.

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@
 set -e
 #set -x
 
+WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 PROJECT_ID=$(gcloud config list project | grep project | awk '{ print $3 }')
-IMAGE_NAME=$(cat manifest.txt | awk '{print $1}')
-IMAGE_TAG=$(cat manifest.txt | awk '{print $2}')
+IMAGE_NAME=$(cat ${WORKDIR}/manifest.txt | awk '{print $1}')
+IMAGE_TAG=$(cat ${WORKDIR}/manifest.txt | awk '{print $2}')
 
 docker build -t us.gcr.io/${PROJECT_ID}/${IMAGE_NAME}:${IMAGE_TAG} . && \
 gcloud docker -- push us.gcr.io/${PROJECT_ID}/${IMAGE_NAME}:${IMAGE_TAG} && \
